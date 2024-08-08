ad39a5b8d9c59eb64a1629c4146274b61cd534e6
David Meier <davidmeier1@gmail.com>
2024-08-08 22:25:08 +0200

    various bugfixes, improvements and flyway scripts

--- src/app/shutters/shutters.component.ts
+++ src/app/shutters/shutters.component.ts
@@ -1,11 +1,9 @@
 import {Component, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
-import {from, Observable, pairwise, toArray} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {map, Observable} from 'rxjs';
 import {ChangeRollerPosition, ChangeRollerState} from '../actions/device.actions';
 import {DeviceState} from '../app.states';
 import {StoreService} from '../store/store.service';
-import {Device} from "../model/device";
 
 export interface ShutterData {
   deviceId: string;
@@ -28,42 +26,19 @@ export class ShuttersComponent implements OnInit {
   displayedColumns: string[] = ['label', 'state', 'position', 'actions', 'lastUpdated'];
 
   constructor(private storeService: StoreService, private store: Store<DeviceState>) {
-    this.rollers$ = this.storeService.getDevicesByApplianceIdentifier('shutter')
-      .pipe(
-        map(devices => devices
-          .map(device => this.getShutterDataObservable(device))),
-        map(shutterDataArrays => shutterDataArrays.flat())
-      )
-  }
-
-  private getShutterDataObservable(device: Device): ShutterData[] {
-    let result: ShutterData[] = [];
-    from(device.properties.filter(prop => prop.type === 'Roller'))
-      .pipe(
-        map(prop => ({
-          label: device.customIdentifiers?.label,
-          state: prop.state,
-          position: prop.positionInPercent,
-          lastUpdated: prop.stateLastUpdated,
-          deviceId: device.id,
-          deviceType: device.type,
-          propertyId: prop.id
-        } as ShutterData)),
-        pairwise(),
-        map(([prev, curr]) => ({
-          label: prev.label,
-          state: prev.state ?? curr.state,
-          position: curr.position ?? prev.position,
-          lastUpdated: curr.lastUpdated ?? prev.lastUpdated,
-          deviceId: prev.deviceId,
-          deviceType: prev.deviceType,
-          propertyId: prev.propertyId
-        } as ShutterData)),
-        toArray()
-      )
-      .subscribe(data => result = data);
-
-    return result;
+    this.rollers$ = storeService.getDevicesByApplianceIdentifier('shutter').pipe(map(devices => devices.map(device =>
+      device.properties.filter(prop => prop.type === 'Roller').map(
+        prop => {
+          return {
+            label: device.customIdentifiers?.label,
+            state: prop.state,
+            position: prop.positionInPercent,
+            lastUpdated: prop.stateLastUpdated,
+            deviceId: device.id,
+            deviceType: device.type,
+            propertyId: prop.id
+          } as ShutterData
+        })).reduce((acc, e) => [...acc, ...e], [])));
   }
 
   ngOnInit(): void {
